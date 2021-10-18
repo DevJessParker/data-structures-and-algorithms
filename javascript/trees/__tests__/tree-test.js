2e1baf918936a55f92c98f21f27b00665f30ebce
DevJessParker <jess.k.dempsey@gmail.com>
2021-10-17 23:27:05 -0700

    Max Value in Tree

--- javascript/trees/__tests__/tree-test.js
+++ javascript/trees/__tests__/tree-test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const BinaryTree = require('../binary-tree.js');
-const BST = require('../binary-search.js');
+const BST = require('../search-tree.js');
 const Node = require('../node.js');
 
 describe('Binary Tree', () => {
@@ -17,29 +17,56 @@ describe('Binary Tree', () => {
   it('should add a left child and right child to a single root node', () => {
     const node1 = new Node(5);
     const tree = new BST(node1);
-
+    
     tree.add(3);
     tree.add(7);
-
-    expect(tree.root.left).toEqual(3);
-    expect(tree.root.right).toEqual(7);
+    
+    expect(tree.root.left.value).toEqual(3);
+    expect(tree.root.right.value).toEqual(7);
   });
 
   it('should successfully return a collection from a preorder traversal', () => {
-    const node1 = new Node(5, 7, 9);
-    const node2 = new Node(10, 12, 14);
+    const node1 = new Node(5);
     const tree = new BST(node1);
-    tree.add(node2);
-    const results = tree._walk();
-    expect(results).toEqual([5, 7, 9, 10, 12, 14]);
+    tree.add(1);
+    tree.add(10);
+    tree.add(3);
+    tree.add(8);
+    const results = tree.preOrder();
+    expect(results).toEqual([5, 1, 3, 10, 8]);
   });
 
   it('should successfully return a collection from a inorder traversal', () => {
-    
+    const node1 = new Node(5);
+    const tree = new BST(node1);
+    tree.add(1);
+    tree.add(10);
+    tree.add(3);
+    tree.add(8);
+    const results = tree.inOrder();
+    expect(results).toEqual([1, 3, 5, 8, 10]);
   })
 
   it('should successfully return a collection from a postorder traversal', () => {
-    
+    const node1 = new Node(5);
+    const tree = new BST(node1);
+    tree.add(1);
+    tree.add(10);
+    tree.add(3);
+    tree.add(8);
+    const results = tree.postOrder();
+    expect(results).toEqual([3, 1, 8, 10, 5]);
+  })
+
+  it('should find the maximum value in a binary tree', () => {
+    const node1 = new Node(5);
+    const tree = new BST(node1);
+    tree.add(1);
+    tree.add(10);
+    tree.add(3);
+    tree.add(8);
+    const results = tree.findMaxValue();
+    expect(results).toEqual(10);
   })
 
 })
