cfeafff8ec16bb5ab8ece6d3369c67fa857eaa69
DevJessParker <jess.k.dempsey@gmail.com>
2021-10-20 00:04:15 -0700

    left-join with two tables

--- javascript/hashtable/repeat-word.js
+++ javascript/hashtable/repeat-word.js
@@ -8,9 +8,6 @@ function repeatedWord(str) {
   let values = str.split(' ');
   values.map(word => table.set(word, word));
 
-  console.log('TABLE', table.map)
-  console.log(table.has('do'));
-
   for (let i = 0; i < values.length; i++) {
     if (table.has(values[i])) {
       return values[i]
